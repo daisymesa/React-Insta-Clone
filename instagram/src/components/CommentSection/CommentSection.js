7a76d0f11e06037b2bfda000e74f14fcf621eb25
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-05-02 19:09:09 -0400

    Adding files needing to be tracked

---
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import dummyData from './././dummy-data.js';
+
+const CommentSection = () => {
+    return (
+        //Main Container div for Comment Section
+
+        <div className="comment-section">
+            <div className="username-section">
+                <p>
+                    <strong>{props.dummyData.comments.username}</strong>
+                </p>
+            </div>
+
+            <div className="comment-text">
+                <p>
+                    {props.dummyData.comments.text}
+                </p>
+            </div>
+
+        </div>
+
+
+        //Container div for Comment Section Ends Here
+    );
+};
+
+
+export default CommentSection;
